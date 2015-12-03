bf37b60167f49ecec8623e5eb7c73c7af79c72fc
Łukasz <ukasz@192.168.56.1>
2015-12-03 23:03:41 +0100

    Zmiana 4 pól na kolekcję.

--- SpinalInjuryVisualAnalisys/src/auxiliary/BMIRangeFactory.java
+++ SpinalInjuryVisualAnalisys/src/auxiliary/BMIRangeFactory.java
@@ -1,21 +1,31 @@
 package auxiliary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BMIRangeFactory
 {
 
 	private static BMIRangeFactory instance = null;
 	
-	private BMIRange underweight;
+	/*private BMIRange underweight;
 	private BMIRange normal;
 	private BMIRange overweight;
-	private BMIRange adiposity;
+	private BMIRange adiposity;*/
+	
+	private List<BMIRange> bmiRanges;
 	
 	private BMIRangeFactory()
 	{
-		underweight = new BMIRange(BMIRangeName.UNDERWEIGHT,Double.MIN_VALUE,18.5);
+		bmiRanges = new ArrayList<>(4);
+		bmiRanges.add(new BMIRange(BMIRangeName.UNDERWEIGHT,Double.MIN_VALUE,18.5));
+		bmiRanges.add(new BMIRange(BMIRangeName.NORMAL,18.5, 25));
+		bmiRanges.add(new BMIRange(BMIRangeName.OVERWEIGHT,25, 30));
+		bmiRanges.add(new BMIRange(BMIRangeName.ADIPOSITY,30, Double.MAX_VALUE));
+		/*underweight = new BMIRange(BMIRangeName.UNDERWEIGHT,Double.MIN_VALUE,18.5);
 		normal = new BMIRange(BMIRangeName.NORMAL,18.5, 25);
 		overweight = new BMIRange(BMIRangeName.OVERWEIGHT,25, 30);
-		adiposity = new BMIRange(BMIRangeName.ADIPOSITY,30, Double.MAX_VALUE);
+		adiposity = new BMIRange(BMIRangeName.ADIPOSITY,30, Double.MAX_VALUE);*/
 
 	}
 	
@@ -32,10 +42,10 @@ public class BMIRangeFactory
 	{
 		switch(bmiRangeName)
 		{
-		case UNDERWEIGHT : return underweight;
-		case NORMAL 	 : return normal;
-		case OVERWEIGHT  : return overweight;
-		case ADIPOSITY   : return adiposity;
+		case UNDERWEIGHT : return bmiRanges.get(0);//underweight
+		case NORMAL 	 : return bmiRanges.get(1);//normal
+		case OVERWEIGHT  : return bmiRanges.get(2);//overweight
+		case ADIPOSITY   : return bmiRanges.get(3);//adiposity
 		default 		 : return null;
 		}
 	}
